b793c5438b830949705e43ef97466624180b141c
nakatsuchi <nakata3415@gmail.com>
2015-07-11 06:16:53 +0900

    revert key

--- migrations/20150710214031_initial_schema.js
+++ migrations/20150710214031_initial_schema.js
@@ -1,12 +1,11 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('jmaxml_feeds', function(table) {
-    table.uuid('uuid').notNull();
+    table.uuid('uuid').primary();
     table.timestamp('updated').notNull();
     table.string('title').notNull();
     table.string('author');
     table.json('doc', true);
-    table.primary(['uuid', 'updated']);
   });
 };
 
